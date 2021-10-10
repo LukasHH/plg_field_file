b19dd1f834b657dc6445589722bf870af8eab7d5
LukasHH <31522294+LukasHH@users.noreply.github.com>
2021-10-10 20:11:38 +0200

    start

---
+++ file.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Plugin Field - File based on Media Manger
+ * ------------------------------------------------------------------------
+ * Author    it-conserv.de
+ * Copyright (C) 2021 it-conserv.de All Rights Reserved.
+ * License - http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * Websites: it-conserv.de
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\Form;
+
+/**
+ * Fields File Plugin
+ *
+ * @since  3.7.0
+ */
+class PlgFieldsFile extends \Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin
+{
+	/**
+	 * Transforms the field into a DOM XML element and appends it as a child on the given parent.
+	 *
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   Form        $form    The form.
+	 *
+	 * @return  DOMElement
+	 *
+	 * @since   4.0.0
+	 */
+	public function onCustomFieldsPrepareDom($field, DOMElement $parent, Form $form)
+	{
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		// see administrator/components/com_media/src/Model/ApiModel.php
+		$fieldNode->setAttribute('type', 'media');
+		$fieldNode->setAttribute('types', 'documents,videos,audios');
+		$fieldNode->setAttribute('preview', 'false');
+
+		return $fieldNode;
+	}
+
+	/**
+	 * Before prepares the field value.
+	 *
+	 * @param   string     $context  The context.
+	 * @param   \stdclass  $item     The item.
+	 * @param   \stdclass  $field    The field.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function onCustomFieldsBeforePrepareField($context, $item, $field)
+	{
+		// Check if the field should be processed by us
+		
+		if (!$this->isTypeSupported($field->type))
+		{
+			return;
+		}		
+
+		// Check if the field value is an old (string) value		
+		$field->value = $this->checkValue($field->value);
+	}
+
+	/**
+	 * Before prepares the field value.
+	 *
+	 * @param   string  $value  The value to check.
+	 *
+	 * @return  array  The checked value
+	 *
+	 * @since   4.0.0
+	 */
+	private function checkValue($value)
+	{
+		json_decode($value);
+
+		if (json_last_error() === JSON_ERROR_NONE)
+		{
+			return (array) json_decode($value, true);
+		}
+
+		return array('file' => $value, 'alt_text' => '');
+	}
+	
+	
+	/**
+	 * Check the filesize
+	 *
+	 * @param   string  $rawvalue
+	 *
+	 * @return  string  The filesize with unit
+	 *
+	 * @since   4.0.0
+	 */	
+	function get_file_size($file)
+	{
+		
+		$filepath	= str_replace('%20', ' ', $file);		
+		$size = filesize($filepath);
+		$units = array("Byte","KB","MB","GB","TB","PB","EB","ZB","YB");
+
+		foreach($units as $pow => $unit) {
+			if ($size / pow(1024, $pow) < 1024)
+			return number_format($size / pow(1024, $pow), 1,  ",", ".") .' ' . $unit;
+		}
+	}
+	
+}
---
+++ tmpl/file.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Plugin Field - File based on Media Manger
+ * ------------------------------------------------------------------------
+ * Author    it-conserv.de
+ * Copyright (C) 2021 it-conserv.de All Rights Reserved.
+ * License - http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * Websites: it-conserv.de
+ */
+ 
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Helper\MediaHelper;
+use Joomla\CMS\Language\Text;
+
+if ($field->value == '')
+{
+	return;
+}
+
+/*
+echo '<pre style="white-space: pre !important;">';
+print_r($item);
+echo '</pre>';
+*/
+
+$value  = $field->rawvalue;
+
+if ($value)
+{
+	
+	// Data
+	$filepath	= str_replace('%20', ' ', $value);
+	$filename	= basename($filepath);
+	$mime		= MediaHelper::getMimeType($filepath);
+	$size		= $this->get_file_size($filepath);
+
+	mt_srand ((double)microtime()*1000000);
+	$id			= mt_rand(1,1000); //erzeuge Zufallszahl
+
+	$html		= '';
+	$inline = '<object width="100%" height="500px" type="'.$mime.'" data="'.$value.'"></object>';
+	$modalId = 'fileModal_'.$id;
+	
+	
+	//Filename
+	if( $fieldParams->get('filename') == '1')
+	{	
+		$html .='<span style="margin-right: 0.5rem;"><strong>'.$filename.' ('.$size.')'.'</strong></span>';
+	}
+	
+	// Modal - Preview
+	if( $fieldParams->get('preview') == '1')
+	{
+		HTMLHelper::_('bootstrap.modal', '.selector', []);
+		$title = $filename;
+		$modalId = 'fileModal_'.$id;
+		
+		
+		$html  .= '
+			<button class="btn btn-primary btn-sm" type="button" data-bs-toggle="modal" data-bs-target="#'.$modalId.'"><span class="icon-search" aria-hidden="true"></span> '. Text::_('PREVIEW') .' </button>
+		';
+		
+		$html .='
+			<div id="'.$modalId.'" class="modal fade" tabindex="-1" aria-labelledby="ModalLabel'.$id.'" aria-hidden="true">
+				<div class="modal-dialog modal-lg modal-fullscreen-lg-down">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 id="ModalLabel'.$id.'" class="modal-title">'.$title.'</h5>
+							<button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							'.$inline.'
+						</div>
+						<div class="modal-footer">
+							<button class="btn btn-secondary" type="button" data-bs-dismiss="modal">'. Text::_('JCLOSE') .'</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		';
+	}
+	
+	//Download	
+	if( $fieldParams->get('download') == '1')
+	{
+		$html .= '<a href="'.$value.'" download="" class="btn btn-primary btn-sm"><span class="icon-file" aria-hidden="true"></span> '. Text::_('DOWNLOAD') .'</a>';	
+	}
+	
+	//rawvalue
+	if( $fieldParams->get('rawvalue') == '1')
+	{
+		$html .= $value;
+	}
+	
+	echo $html;
+}
